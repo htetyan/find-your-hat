5438054be41ea17a62c9d6a8b9d8387b7c2148fc
Htet Yan <htet.hw.yan@gmail.com>
2021-12-12 20:56:59 +0800

    print method

--- main.js
+++ main.js
@@ -6,5 +6,24 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  
+  constructor(fieldArray){
+      this.field = fieldArray;	
 }
+
+	print(){
+		let fieldString = '';
+		for (let i = 0; i < this.field.length; i++) {
+			this.field[i].forEach(elem => fieldString += elem)
+			fieldString += '\n'; 
+	 		}
+		console.log(fieldString);
+		}
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
