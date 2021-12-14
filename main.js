77998bca27c72adc2906c7689b68156c415fe34c
Htet Yan <htet.hw.yan@gmail.com>
2021-12-14 22:42:58 +0800

    Print Methods
    Main.js File:
    - two different print methods
    
    App.js File:
    Source file: https://gist.github.com/cford-14/0ff506149b60856cbc48e5e3c1d231ce
    Another students solution file, needs to change file to play the game.
    So far it just prints plain 'fields'.

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
 class Field {
-  constructor(fieldArray){
-      this.field = fieldArray;	
-}
+  constructor(fieldArray) {
+    this.field = fieldArray;
+  }
 
-	print(){
-		let fieldString = '';
-		for (let i = 0; i < this.field.length; i++) {
-			this.field[i].forEach(elem => fieldString += elem)
-			fieldString += '\n'; 
-	 		}
-		console.log(fieldString);
-		}
+  print() {
+    let fieldString = "";
+    for (let i = 0; i < this.field.length; i++) {
+      this.field[i].forEach((elem) => (fieldString += elem));
+      fieldString += "\n";
+    }
+    console.log(fieldString);
+  }
+
+  print2() {
+    let fieldString = '';
+    for (let i = 0; i < this.field.length; i++) {
+      fieldString += this.field[i].join('');
+      fieldString += '\n';
+    }
+    console.log(fieldString);
+  }
 }
 
 const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
 ]);
 
 myField.print();
+myField.print2();
