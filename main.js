94895b2b6c596cf361e0b9bc734761da261df990
Htet Yan <htet.hw.yan@gmail.com>
2021-12-29 15:10:46 +0800

    array tile functions

--- main.js
+++ main.js
@@ -80,6 +80,16 @@ function printTileLine2(size) {
 }
 //console.log(printTileLine(8));
 
+function printTileArrayLine(size) {
+    let helperArray = [];
+    for (let i=0;i<size;i++) {
+        helperArray.push(tileRandom());
+    }
+    return helperArray;
+}
+
+//console.log(printTileArrayLine(10));
+
 function printHatLine(size) {
     let finalLine = "";
     let helperArray = [];
@@ -98,6 +108,22 @@ function printHatLine(size) {
 
 //console.log(printHatLine(10));
         
+function printHatArrayLine(size) {
+    let helperArray = [];
+    let hatPositionNumber = Math.floor(Math.random()*size)-1; 
+    for (let i=0; i<size; i++) {
+        if ( i === hatPositionNumber) {
+            helperArray.push(hat);
+        } else {
+            helperArray.push(tileRandom());
+        }
+    }
+   // console.log(helperArray.length);
+    return helperArray;
+};
+
+//console.log(printHatArrayLine(10));
+
 function printTileGrid(length, width) {
     let finalGrid = "";
     let hatLineNum = Math.floor(Math.random()*length);
@@ -109,8 +135,61 @@ function printTileGrid(length, width) {
     }
     return finalGrid;
 }
-
+/*
 console.log(printTileGrid(10, 10));
 console.log(printTileGrid(5, 5));
 console.log(printTileGrid(8, 11));
+*/
 
+let myArrayGrid = [];
+
+function printTileArrayGrid(length, width) {
+    let finalArrayGrid = [];
+    let hatLineNum = Math.floor(Math.random()*length);
+    for (let i=0;i<length;i++) {
+        if ( i === hatLineNum) {
+            finalArrayGrid.push(printHatArrayLine(width));
+        }
+        finalArrayGrid.push(printTileArrayLine(width));
+    }
+    myArrayGrid = finalArrayGrid;
+}
+
+//console.log(myArrayGrid);
+printTileArrayGrid(8,12);
+console.log(myArrayGrid);
+console.log(myArrayGrid[0][0]);
+//console.log(myArrayGrid[7][11]);
+//console.log(myArrayGrid[2][5]);
+
+//let letter = prompt("Enter a letter : ");
+//console.log(`Here is your ${letter}`);
+
+function xLocation(array, p1, p2) {
+    if ((p1 && p2) === undefined)
+        p1 = 0;
+        p2 = 0;
+    if ((p1 || p2) === undefined)
+        p1 = 0;
+        p2 = 0;
+    return array[p1][p2];
+}
+
+console.log(xLocation(myArrayGrid));
+console.log(xLocation(myArrayGrid, 1));
+
+function inputProcessor(userInput) {
+    if (userInput === "h") {
+        //move left
+        //change the index - 1
+        //update the moving board
+    } else if (userInput === "j") {
+        //functions
+    } else if (userInput === "k") {
+        //functions
+    } else if (userInput === "l") {
+        //functions
+    } else {
+      console.log("Please put in the correct direction inputs: h,j,k or l")
+    }
+}
